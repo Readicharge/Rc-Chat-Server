4f2525707b2bf5e0c073de6146f44f21d60add98
Readicharge <Readicharge@gmail.com>
2024-01-08 11:25:08 +0530

    ......

--- server.js
+++ server.js
@@ -15,19 +15,21 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Middlewares
-app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
-app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
+
 
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
--- socket/socket.js
+++ socket/socket.js
@@ -3,6 +3,7 @@ import http from "http";
 import express from "express";
 import Message from "../models/messageModel.js";
 import Conversation from "../models/conversationModel.js";
+import Ably from "ably";
 
 const app = express();
 const server = http.createServer(app);
@@ -13,35 +14,42 @@ const io = new Server(server, {
   },
 });
 
+const ably = new Ably.Realtime("RiBQGw.vcnBXw:I1tNRwt-47uXrwOcHbs-koOfWDUub9whWEcWWBsPk7A");
 
 export const getRecipientSocketId = (recipientId) => {
-	return userSocketMap[recipientId];
+  return userSocketMap[recipientId];
 };
 
 const userSocketMap = {}; // userId: socketId
 
 io.on("connection", (socket) => {
-	console.log("user connected", socket.id);
-	const userId = socket.handshake.query.userId;
+  console.log("user connected", socket.id);
+  const userId = socket.handshake.query.userId;
 
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
 
-	socket.on("markMessagesAsSeen", async ({ conversationId, userId }) => {
-		try {
-			await Message.updateMany({ conversationId: conversationId, seen: false }, { $set: { seen: true } });
-			await Conversation.updateOne({ _id: conversationId }, { $set: { "lastMessage.seen": true } });
-			io.to(userSocketMap[userId]).emit("messagesSeen", { conversationId });
-		} catch (error) {
-			console.log(error);
-		}
-	});
+  const channel = ably.channels.get(userId);
 
-	socket.on("disconnect", () => {
-		console.log("user disconnected");
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+  channel.subscribe("markMessagesAsSeen", async ({ conversationId, userId }) => {
+    try {
+      await Message.updateMany(
+        { conversationId: conversationId, seen: false },
+        { $set: { seen: true } }
+      );
+      await Conversation.updateOne(
+        { _id: conversationId },
+        { $set: { "lastMessage.seen": true } }
+      );
+      io.to(userSocketMap[userId]).emit("messagesSeen", { conversationId });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    delete userSocketMap[userId];
+  });
 });
 
 export { io, server, app };
